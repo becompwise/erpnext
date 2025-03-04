18f7a83e4f7bcbcdba7dfab9bf3c080c3a65c0a6
becompwise <becompwise@gmail.com>
2025-03-03 17:09:42 -0800

    ...

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -23,7 +23,7 @@ def get_default_company(user=None):
 	return default_company
 
 
-def get_default_currency():
+def get_default_currency(): 
 	"""Returns the currency of the default company"""
 	company = get_default_company()
 	if company:
